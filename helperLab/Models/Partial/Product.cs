ed54f2da2e54330d44f41f86020f71dc47d2a929
Liang Zhao <zhaoliangbz@gmail.com>
2017-10-03 13:09:07 -0700

    1st

---
+++ helperLab/Models/Partial/Product.cs
@@ -0,0 +1,52 @@
+﻿using helperLab.Models.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace helperLab.Models.Northwind
+{
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product { }
+
+    public class ProductMetaData
+    {
+        [HiddenInput(DisplayValue = false)]
+        public object ProductID { get; set; }
+
+        [Required(ErrorMessage = "Product name is required.")]
+        [Display(Name = "Product Name")]
+        [ExcludeLength(3, ErrorMessage = "Name should be less than 3 chars!")]
+        public object ProductName { get; set; }
+
+        [Required(ErrorMessage = "Unit price is required.")]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Unit Price")]
+        public object UnitPrice { get; set; }
+
+        [ScaffoldColumn(false)]
+        public object ReorderLevel { get; set; }
+
+        [UIHint("CategoryDropDown")]
+        [Display(Name = "Category")]
+        public object CategoryID { get; set; }
+
+        [UIHint("SupplierDropDown")]
+        [Display(Name = "Supplier")]
+        public object SupplierID { get; set; }
+
+        [ScaffoldColumn(false)]
+        public object UnitsInStock { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string QuantityPerUnit { get; set; }
+
+        [ScaffoldColumn(false)]
+        public object UnitsOnOrder { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool Discontinued { get; set; }
+    }
+}
---
+++ helperLab/Models/Validation/ExcludeLength.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace helperLab.Models.Validation
+{
+    public class ExcludeLength : ValidationAttribute
+    {
+        private readonly int _len;
+
+        public ExcludeLength(int length) : base("{0} has invalid length.")
+        {
+            _len = length;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string val = value.ToString();
+                if (val.Length > _len)
+                {
+                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
